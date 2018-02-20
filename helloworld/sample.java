c3dfb1d0a4962c6443eed42930acdb35ad0bc546
eri5 <erikonishida5@gmail.com>
2018-02-20 19:31:19 +0900

    別ブランチにコミットしてみた

--- helloworld/sample.java
+++ helloworld/sample.java
@@ -1,5 +1,13 @@
+import java.io.*;
+
 class sample{
-  public static void main(String args[]){
+  public static void main(String args[]) throws IOException {
     System.out.println("Helloworld");
+  	
+  	InputStreamReader is = new InputStreamReader(System.in);       
+    BufferedReader br = new BufferedReader(is); 
+  	
+  	String str = br.readLine();                                    
+    System.out.println(str + "‚ª“ü—Í‚³‚ê‚Ü‚µ‚½.");
   }
 }
